0bbe3e7942316753abf111fd0e27bc9ac6009992
Terry Zhuo <terryzhuo25@gmail.com>
2024-04-20 02:49:33 +1000

    update: workflows

--- script/run.sh
+++ script/run.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-NAMES=(
-    chien
-    jenny
-    wenhao
-    ratna
-    niklas
-)
+NAMES=(chien jenny wenhao ratna niklas hanhu)
 for name in "${NAMES[@]}"; do
     cp data/raw/*"$name"*py data/clean
 done
 python script/parse.py
 for file in data/processed/*wo_doc.py; do
+    if ! grep -q f_855 "$file"; then
+        continue
+    fi
+    
     if ! pytest "$file"; then
         echo "Pytest failed on $file, stopping..."
         exit 1
