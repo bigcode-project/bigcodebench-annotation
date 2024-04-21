6878ec62db7b8433b8fe76a912dfdd796c73c2b0
Terry Zhuo <terryzhuo25@gmail.com>
2024-04-21 21:28:25 +1000

    update: workflows for 359

--- script/run.sh
+++ script/run.sh
@@ -1,24 +1,26 @@
-NAMES=(chien jenny wenhao niklas hanhu ming wending)
+NAMES=(chien jenny wenhao niklas hanhu ming)
 for name in "${NAMES[@]}"; do
     cp data/raw/*"$name"*py data/clean
 done
 python script/parse.py
-for name in "${NAMES[@]}"; do
-    for file in data/processed/*"$name"*wo_doc.py; do
-        if [[ $file == *"f_855"* ]]; then
-            continue
-        fi
 
-        if ! pytest "$file"; then
-            echo "Pytest failed on $file, stopping..."
-            exit 1
-        fi
-    done
+pytest data/processed/f_359*.py
+# for name in "${NAMES[@]}"; do
+#     for file in data/processed/*"$name"*wo_doc.py; do
+#         if [[ $file == *"f_855"* ]]; then
+#             continue
+#         fi
 
-    for file in data/processed/*"$name"*w_doc.py; do
-        if ! pytest --doctest-modules "$file"; then
-            echo "Pytest failed on $file, stopping..."
-            exit 1
-        fi
-    done
-done
+#         if ! pytest "$file"; then
+#             echo "Pytest failed on $file, stopping..."
+#             exit 1
+#         fi
+#     done
+
+#     for file in data/processed/*"$name"*w_doc.py; do
+#         if ! pytest --doctest-modules "$file"; then
+#             echo "Pytest failed on $file, stopping..."
+#             exit 1
+#         fi
+#     done
+# done
