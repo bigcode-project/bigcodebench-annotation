6840953af0d25181138c2276c5804f27c6c333c8
Terry Zhuo <terryzhuo25@gmail.com>
2024-04-22 00:57:52 +1000

    update: 593

--- data/raw/f_593_niklas.py
+++ data/raw/f_593_niklas.py
@@ -49,6 +49,7 @@ def run_tests():
     runner.run(suite)
 
 class TestCases(unittest.TestCase):
+    
     def test_case_1(self):
         data = np.array([[1, 4, 0], [2, 5, 1], [3, 6, 0]])
         columns = ['A', 'B', 'C']
@@ -56,23 +57,48 @@ class TestCases(unittest.TestCase):
 
     def test_case_2(self):
         data = np.array([[1, 2, 3, -10], [4, 5, 6, -10], [1, 1, 1, 0]])
-        columns = ['A', 'B', 'C']
-        self.assertEqual(f_593(data, columns, 'C'), 1.0)
+        columns = ['A', 'B', 'C', 'D']
+        self.assertEqual(f_593(data, columns, 'C'), 0.0)
 
     def test_case_3(self):
-        data = np.array([[1, 2, 3, -10], [4, 5, 6, -10], [0, 0, 0, 1]])
+        data = np.array([
+            [60, 45, 1],
+            [40, 55, 1],
+            [30, 71, 1],
+            [20, 82, 1],
+            [10, 95, 1],
+            [59, 40, 0],
+            [39, 60, 1],
+            [29, 70, 1],
+            [19, 80, 1],
+            [9,  89, 1]
+        ])
         columns = ['A', 'B', 'C']
         self.assertEqual(f_593(data, columns, 'C'), 1.0)
 
     def test_case_4(self):
-        data = np.array([[-10, 2, 3, -10], [-10, 5, 6, -10], [1, 0, 0, 1]])
-        columns = ['A', 'B', 'C']
-        self.assertEqual(f_593(data, columns, 'C'), 1.0)
+        data = np.array([
+            [-10, 2, 3, -10],
+            [-10, 5, 6, 10],
+            [-10, -2, -1, -10],
+            [-10, 1, 0, -10],
+            [-10, 8, 9, 10],
+            [-10, -5, -4, -10]
+        ])
+        columns = ['A', 'B', 'C', 'D']
+        self.assertEqual(f_593(data, columns, 'D'), 1.0)
 
     def test_case_5(self):
-        data = np.array([[-10, 2, 3, -10], [-10, 5, 6, -10], [0, 1, 1, 0]])
-        columns = ['A', 'B', 'C']
-        self.assertEqual(f_593(data, columns, 'C'), 1.0)
+        data = np.array([
+            [-10, 2, 3, -10, 1],
+            [-10, 5, 6, 10, 1],
+            [-10, -2, -1, -10, 1],
+            [-10, 1, 0, -10, 1],
+            [-10, 8, 9, 10, 1],
+            [-10, -5, -4, -10, 1]
+        ])
+        columns = ['A', 'B', 'C', 'D', 'E']
+        self.assertEqual(f_593(data, columns, 'D'), 1.0)
 
 if __name__ == "__main__":
     run_tests()
