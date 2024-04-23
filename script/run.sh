52b80bd3a4a6f4cff454e8fe85d9e48748b53bb1
Terry Zhuo <terryzhuo25@gmail.com>
2024-04-24 03:16:58 +1000

    update: workflows

--- script/run.sh
+++ script/run.sh
@@ -2,6 +2,7 @@ NAMES=(chien jenny wenhao niklas hanhu ratna ming)
 for name in "${NAMES[@]}"; do
     cp data/raw/*"$name"*py data/clean
 done
+flake8 data/clean/*.py --select=E9,F63,F7,F82 --show-source --statistics
 python script/parse.py
 
 for name in "${NAMES[@]}"; do
