459b33135f91e77b06249cb356f6afc49a4df81a
Simon Brunner <mysterioso+github@mailbox.org>
2024-05-02 20:25:33 +0200

    add explicit second library usage

--- data/raw/f_676_simon.py
+++ data/raw/f_676_simon.py
@@ -2,37 +2,44 @@ import pandas as pd
 import random
 
 
-def f_676(df, item, seed):
+def f_676(dictionary, item, seed):
     """
-    Find the locations of a particular item in a pandas DataFrame.
+    Converts a dictionary to a pandas DataFrame and find the locations of a particular item in the resulting DataFrame.
     Counts the number of occurences and adds a random integer x, where 0 <=x < 10, to it.
 
     Parameters:
-    df (DataFrame): The DataFrame to search.
+    dict (dictionary): The dictionary to search.
     item (str): The item to find.
     seed(int): seed for random number generation.
 
     Returns:
     list: A list of tuples. Each tuple contains the row-index and column-name where the item is found.
     int: The number of occurences with the added random number.
+    DataFrame: The converted dictionary.
 
     Requirements:
     - pandas
     - random
 
     Example:
-    >>> df = pd.DataFrame({'A': ['apple', 'banana'], 'B': ['orange', 'apple']})
-    >>> f_676(df, 'apple', seed=12)
-    ([(0, 'A'), (1, 'B')], 9)
-
-    >>> df = pd.DataFrame({'A': ['a', 'b', 'e'], 'B': ['c', 'd', 'd'], '2': ['asdf', 'ddd', 'aaaa'], '12': ['e', 'e', 'd']})
-    >>> f_676(df, 'e', seed=2)
-    ([(2, 'A'), (0, '12'), (1, '12')], 3)
+    >>> dict = {'A': ['apple', 'banana'], 'B': ['orange', 'apple']}
+    >>> f_676(dict, 'apple', seed=12)
+    ([(0, 'A'), (1, 'B')], 9,         A       B
+    0   apple  orange
+    1  banana   apple)
+    
+    >>> dict = {'A': ['a', 'b', 'e'], 'B': ['c', 'd', 'd'], '2': ['asdf', 'ddd', 'aaaa'], '12': ['e', 'e', 'd']}
+    >>> f_676(dict, 'e', seed=2)
+    ([(2, 'A'), (0, '12'), (1, '12')], 3,    A  B     2 12
+    0  a  c  asdf  e
+    1  b  d   ddd  e
+    2  e  d  aaaa  d)
     """
     random.seed(seed)
     random_int = random.randint(0, 9)
+    df = pd.DataFrame(dictionary)
     positions = [(index, col) for col in df for index, val in enumerate(df[col]) if val == item]
-    return positions, len(positions) + random_int 
+    return positions, len(positions) + random_int , df
 
 import unittest
 import pandas as pd
@@ -49,51 +56,58 @@ def run_tests():
 class TestCases(unittest.TestCase):
     
     def test_case_1(self):
-        # Simple DataFrame
-        df = pd.DataFrame({'A': ['apple', 'banana'], 'B': ['orange', 'apple']})
-        result, count = f_676(df, 'apple', 2222)
+        # Simple dict
+        dictionary = {'A': ['apple', 'banana'], 'B': ['orange', 'apple']}
+        result, count, df = f_676(dictionary, 'apple', 2222)
         expected_result = [(0, 'A'), (1, 'B')]
         self.assertCountEqual(result, expected_result)
         self.assertEqual(count, 5)
+        pd.testing.assert_frame_equal(pd.DataFrame(dictionary), df)
 
     def test_case_2(self):
         # No occurrence of the item
-        df = pd.DataFrame({'A': ['orange', 'banana'], 'B': ['orange', 'banana']})
-        result, count = f_676(df, 'apple', seed=12)
+        dictionary = {'A': ['orange', 'banana'], 'B': ['orange', 'banana']}
+        result, count, df = f_676(dictionary, 'apple', seed=12)
         expected_result = []
         self.assertCountEqual(result, expected_result)
         self.assertEqual(count, 7)
-  
+        pd.testing.assert_frame_equal(pd.DataFrame(dictionary), df)
+
     def test_case_3(self):
-        # Larger DataFrame
+        # Larger dict
         fake.random.seed(111)
-        df = pd.DataFrame({
+        dictionary = {
             'A': [fake.random_element(elements=('apple', 'banana', 'orange')) for _ in range(10)],
             'B': [fake.random_element(elements=('apple', 'banana', 'orange')) for _ in range(10)],
             'C': [fake.random_element(elements=('apple', 'banana', 'orange')) for _ in range(10)]
-        })
-        result, count = f_676(df, 'apple', seed=22)
+        }
+        result, count, df = f_676(dictionary, 'apple', seed=22)
         expected_result = [(index, col) for col in df for index, val in enumerate(df[col]) if val == 'apple']
         self.assertCountEqual(result, expected_result)
         self.assertEqual(count, 10)
+        pd.testing.assert_frame_equal(pd.DataFrame(dictionary), df)
+
     
     def test_case_4(self):
-        # Empty DataFrame
-        df = pd.DataFrame()
-        result, count = f_676(df, 'apple', seed=112)
+        # Empty dict
+        dictionary = {}
+        result, count, df = f_676(dictionary, 'apple', seed=112)
         expected_result = []
         self.assertCountEqual(result, expected_result)
         self.assertEqual(count, 7)
+        pd.testing.assert_frame_equal(pd.DataFrame(dictionary), df)
 
     def test_case_5(self):
-        # DataFrame with non-string values
-        df = pd.DataFrame({
+        # dict with non-string values
+        dictionary = {
             'A': [1, 2, 3, 4, 5],
             'B': [2, 3, 4, 5, 6]
-        })
-        result, count = f_676(df, 3, seed=32)
+        }
+        result, count, df = f_676(dictionary, 3, seed=32)
         expected_result = [(2, 'A'), (1, 'B')]
         self.assertCountEqual(result, expected_result)
         self.assertEqual(count, 3)
+        pd.testing.assert_frame_equal(pd.DataFrame(dictionary, df))
+
 if __name__ == "__main__":
     run_tests()
--- data/raw/f_681_simon.py
+++ data/raw/f_681_simon.py
@@ -4,17 +4,19 @@ import numpy as np
 
 def f_681(test_scores, student):
     """
-    Calculate the average test score and the standard deviation for a particular student from a given DataFrame of test results.
+    Convert a dictionary of test results into a pandas DataFrame and
+    Calculate the average test score and the standard deviation for a particular student from this DataFrame.
     
     Parameters:
-    test_scores (DataFrame): The DataFrame containing columns 'Student' and 'Score'.
-        The Student columns is of dtype int and contains student IDs. The Score 
-        column is of dtype float.
+    test_scores (dictionary): The dictionary containing keys 'Student' and 'Score'.
+        The Student values are of dtype int and contain student IDs. The Score 
+        values are of dtype float.
     student (int): The specific student ID for which the average score needs to be calculated.
     
     Returns:
     np.array([float, float]): A numpy array containing the average score and the standard deviation for the student.
-    
+    DataFrame: the converted dictionary.
+
     Raises:
     ValueError: student is not present in the test_scores dataframe
                 
@@ -25,23 +27,77 @@ def f_681(test_scores, student):
     Example:
     >>> STUDENTS = range(1, 101)
     >>> np.random.seed(10)
-    >>> scores = pd.DataFrame({'Student': list(np.random.choice(STUDENTS, 50, replace=True)), 
-    ...                        'Score': np.random.randint(50, 101, size=50)})
+    >>> scores = {'Student': list(np.random.choice(STUDENTS, 50, replace=True)), 
+    ...                        'Score': np.random.randint(50, 101, size=50)}
     >>> f_681(scores, 10)
-    array([70.        ,  7.07106781])
+    (array([70.        ,  7.07106781]),     Student  Score
+    0        10     65
+    1        16     68
+    2        65     66
+    3        29     57
+    4        90     74
+    5        94     61
+    6        30     67
+    7         9     96
+    8        74     57
+    9         1     61
+    10       41     78
+    11       37     83
+    12       17     70
+    13       12     82
+    14       55     74
+    15       89     94
+    16       63     55
+    17       34     54
+    18       73     57
+    19       79     74
+    20       50     74
+    21       52    100
+    22       55     94
+    23       78     84
+    24       70     90
+    25       14     65
+    26       26     63
+    27       14     74
+    28       93     65
+    29       87     56
+    30       31     71
+    31       31     92
+    32       90     72
+    33       13     61
+    34       66     98
+    35       32     62
+    36       58     78
+    37       37     82
+    38       28     99
+    39       19     65
+    40       94     94
+    41       78     90
+    42       23     92
+    43       24     95
+    44       95     93
+    45       12     83
+    46       29    100
+    47       75     95
+    48       89     90
+    49       10     75)
 
-    >>> scores = pd.DataFrame({'Student': [1, 2, 1, 1], 'Score': [10, 1, 1, 1]})
+    >>> scores = {'Student': [1, 2, 1, 1], 'Score': [10, 1, 1, 1]}
     >>> f_681(scores, 1)
-    array([4.        , 5.19615242])
+    (array([4.        , 5.19615242]),    Student  Score
+    0        1     10
+    1        2      1
+    2        1      1
+    3        1      1)
     """
+    test_scores = pd.DataFrame(test_scores)
     if student not in test_scores['Student'].values:
         raise ValueError(f"The student with ID {student} is not present in the test scores DataFrame.")
-        
     student_scores = test_scores[test_scores['Student'] == student]['Score']
     average_score = student_scores.mean()
     std = student_scores.std()
     
-    return np.array([average_score, std])
+    return np.array([average_score, std]), test_scores
 
 import unittest
 from faker import Faker
@@ -62,21 +118,24 @@ class TestCases(unittest.TestCase):
     def setUp(self):
         self.student_ids = range(1, 6)
         self.students_sample = list(np.random.choice(self.student_ids, 50, replace=True))
-        self.scores = pd.DataFrame({
+        self.scores = {
             'Student': self.students_sample, 
-            'Score': np.random.randint(50, 101, size=50)
-        })
+            'Score': list(np.random.randint(50, 101, size=50))
+        }
 
     def test_case_1(self):
         student_id = self.students_sample[0]
-        expected_avg = self.scores[self.scores['Student'] == student_id]['Score'].mean()
-        expected_std = self.scores[self.scores['Student'] == student_id]['Score'].std()
+        scores_df = pd.DataFrame(self.scores)
+        expected_avg = scores_df[scores_df['Student'] == student_id]['Score'].mean()
+        expected_std = scores_df[scores_df['Student'] == student_id]['Score'].std()
 
-        res = f_681(self.scores, student_id)
+        res, df = f_681(self.scores, student_id)
         avg, std = res
         self.assertIsInstance(res, np.ndarray)
         self.assertAlmostEqual(expected_avg, avg, places=2)
         self.assertAlmostEqual(expected_std, std, places=2)
+        pd.testing.assert_frame_equal(pd.DataFrame(self.scores), df)
+
 
 
     def test_case_2(self):
@@ -85,35 +144,40 @@ class TestCases(unittest.TestCase):
             f_681(self.scores, student_id)
 
     def test_case_3(self):
-        empty_df = pd.DataFrame(columns=['Student', 'Score'])
+        empty_df = dict.fromkeys(['Student', 'Score'])
         student_id = fake.random_int(min=1, max=100)
         with self.assertRaises(ValueError):
             f_681(empty_df, student_id)
 
     def test_case_4(self):
-        scores = pd.DataFrame({
+        scores = {
             'Student': list(self.student_ids), 
             'Score': [100] * len(self.student_ids)
-        })
+        }
         student_id = self.student_ids[3]
-        res = f_681(scores, student_id)
+        res, df = f_681(scores, student_id)
         avg, std = res
         self.assertIsInstance(res, np.ndarray)
         self.assertEqual(avg, 100.0)
         self.assertTrue(np.isnan(std))
+        pd.testing.assert_frame_equal(pd.DataFrame(scores), df)
+
 
     def test_case_5(self):
-        scores = pd.DataFrame({
+        scores = {
             'Student': list(self.student_ids) * 10, 
             'Score': list(np.random.randint(50, 101, size=len(self.student_ids)*10))
-        })
+        }
         student_id = self.student_ids[4]
-        expected_avg = scores[scores['Student'] == student_id]['Score'].mean()
-        expected_std = scores[scores['Student'] == student_id]['Score'].std()
+        scores_df = pd.DataFrame(scores)
+        expected_avg = scores_df[scores_df['Student'] == student_id]['Score'].mean()
+        expected_std = scores_df[scores_df['Student'] == student_id]['Score'].std()
+        res, df = f_681(scores, student_id)
+        avg, std = res
 
-        avg, std = f_681(scores, student_id)
         self.assertAlmostEqual(expected_avg, avg, places=2)
         self.assertAlmostEqual(expected_std, std, places=2)
+        pd.testing.assert_frame_equal(pd.DataFrame(scores), df)
 
 
 
