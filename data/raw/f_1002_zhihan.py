3d1d11e81b8549352100116d0cb58f959836b902
imamnurby <inby04@gmail.com>
2024-05-06 23:37:57 +0000

    remove comments

--- data/raw/f_1002_zhihan.py
+++ data/raw/f_1002_zhihan.py
@@ -1,9 +1,6 @@
 import random
 import statistics
 
-# Constants
-# LETTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
 def f_1002(LETTERS):
     """
     Create a dictionary in which keys are random letters and values are lists of random integers.
