0f83cc7197e30412513f50da4aa595eeeba4a3fc
ratnadiraw <ratnadiraw@gmail.com>
2024-05-07 14:24:21 +0800

    [data] mod script

--- script/run.sh
+++ script/run.sh
@@ -1,7 +1,11 @@
 # NAMES=(chien jenny wenhao niklas hanhu ratna simon ming)
-NAMES=(zhihan)
-for name in "${NAMES[@]}"; do
-    cp data/raw/*"$name"*py data/clean
+# NAMES=(zhihan)
+# for name in "${NAMES[@]}"; do
+#     cp data/raw/*"$name"*py data/clean
+# done
+for i in {932..950}
+do
+    cp data/raw/f_"$i"_zhihan.py data/clean
 done
 flake8 data/clean/*.py --select=E9,F63,F7,F82 --show-source --statistics
 python script/parse.py
