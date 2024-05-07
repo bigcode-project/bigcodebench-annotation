bb452a1bdd3fc18f352fff49a4ccdac48c5c5746
Terry Zhuo <terryzhuo25@gmail.com>
2024-05-08 01:06:00 +1000

    fix: skip 2248 due to github connection to geoapi

--- script/run.sh
+++ script/run.sh
@@ -6,6 +6,7 @@ flake8 data/clean/*.py --select=E9,F63,F7,F82 --show-source --statistics
 python script/parse.py
 
 for name in "${NAMES[@]}"; do
+
     for file in data/processed/*"$name"*wo_doc.py; do
 
         if ! pytest "$file"; then
@@ -15,6 +16,11 @@ for name in "${NAMES[@]}"; do
     done
 
     for file in data/processed/*"$name"*w_doc.py; do
+        # check skip any file with "f_2248_hanhu" inside
+        if [[ "$file" == *"f_2248_hanhu"* ]]; then
+            continue
+        fi
+
         if ! pytest --doctest-modules "$file"; then
             echo "Pytest failed on $file, stopping..."
             exit 1
