fbbd617fe9aa1557a43587bb06182c141ff7eedb
Ubuntu <ubuntu@ip-172-31-73-55.ec2.internal>
2024-05-10 15:02:47 +0000

    Single split action trigger

--- script/run.sh
+++ script/run.sh
@@ -1,5 +1,5 @@
 # 
-NAMES=(indraneil chien jenny wenhao niklas hanhu ratna simon ming zhihan james xiaoheng armel)
+NAMES=(indraneil) #chien jenny wenhao niklas hanhu ratna simon ming zhihan james xiaoheng armel)
 rm -rf data/clean/*
 for name in "${NAMES[@]}"; do
     # Copy all files for other names
